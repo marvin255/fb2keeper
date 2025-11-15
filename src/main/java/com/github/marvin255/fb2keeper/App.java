ccef28ef758537c5fe8664dfcad945b6200c4fbc
marvin255 <postbox433@gmail.com>
2025-11-15 12:20:02 +0100

    Add basic maven environment

---
+++ src/main/java/com/github/marvin255/fb2keeper/App.java
@@ -0,0 +1,8 @@
+package com.github.marvin255.fb2keeper;
+
+public final class App
+{
+    public static void main(String[] args)
+    {
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.github.marvin255.fb2keeper {
+    requires org.jsoup;
+}

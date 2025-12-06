631bd22d789c48718b4ef7c52ff1935c548c84ad
marvin255 <postbox433@gmail.com>
2025-12-06 23:33:07 +0100

    Add App code

--- src/main/java/com/github/marvin255/fb2keeper/App.java
+++ src/main/java/com/github/marvin255/fb2keeper/App.java
@@ -1,8 +1,25 @@
 package com.github.marvin255.fb2keeper;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.logging.Logger;
+
 public final class App
 {
-    public static void main(String[] args)
+    private static final Fb2Keeper KEEPER = new Fb2Keeper(
+            List.of(
+                    new OperationCopyFileToTarget(),
+                    new OperationZipFb2File()
+            ),
+            Logger.getLogger(App.class.getName())
+    );
+
+    public static void main(String[] args) throws IOException
     {
+        KEEPER.keep(
+                Path.of(args[0]),
+                Path.of(args[1])
+        );
     }
 }
